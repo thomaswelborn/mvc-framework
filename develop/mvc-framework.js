1c44c622e798d87e7dd410866d049e8c8bade838
thomaswelborn <35278795+thomaswelborn@users.noreply.github.com>
2018-02-12 14:30:48 -0800

    Update Events class off method.

--- develop/mvc-framework.js
+++ develop/mvc-framework.js
@@ -9,12 +9,14 @@ class Events {
   off(eventName, callback) {
     var currentEvents = this.events[eventName];
     if(typeof currentEvents === 'undefined' || currentEvents.length === 0) return;
-    var currentEventIndex = Object.entries(currentEvents).map(function(currentEvent, currentEventIndex) {
-      if(typeof callback === 'string' && callback === currentEvent[1].name) return currentEventIndex;
-      if(typeof callback === 'function' && callback.name === currentEvent[1].name) return currentEventIndex;
+    var currentEventIndices = Object.entries(currentEvents).map(function(currentEvent, currentEventIndex) {
+      if(
+        (typeof callback === 'string' && callback === currentEvent[1].name) || 
+        (typeof callback === 'function' && callback.name === currentEvent[1].name)
+      ) return currentEventIndex;
     }.bind(this));
-    for(var key = currentEventIndex.length; key > 0; key--) {
-      currentEvents.splice(currentEventIndex[key], 1);
+    for(var key = currentEventIndices.length; key > 0; key--) {
+      currentEvents.splice(currentEventIndices[key - 1], 1);
     }
   }
   trigger(eventName, data) {
