275fa11be8c551a555707b12f04d58a53185b197
thomaswelborn <35278795+thomaswelborn@users.noreply.github.com>
2018-02-12 14:07:29 -0800

    Events class off method becomes more permissive.
    Accepts string function name and function object.

--- develop/mvc-framework.js
+++ develop/mvc-framework.js
@@ -9,8 +9,13 @@ class Events {
   off(eventName, callback) {
     var currentEvents = this.events[eventName];
     if(typeof currentEvents === 'undefined' || currentEvents.length === 0) return;
-    var currentEventIndex = currentEvents.indexOf(callback);
-    if(currentEventIndex >= 0) currentEvents.splice(currentEventIndex, 1);
+    var currentEventIndex = Object.entries(currentEvents).map(function(currentEvent, currentEventIndex) {
+      if(typeof callback === 'string' && callback === currentEvent[1].name) return currentEventIndex;
+      if(typeof callback === 'function' && callback.name === currentEvent[1].name) return currentEventIndex;
+    }.bind(this));
+    for(var key = currentEventIndex.length; key > 0; key--) {
+      currentEvents.splice(currentEventIndex[key], 1);
+    }
   }
   trigger(eventName, data) {
     try {
