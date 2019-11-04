b14c000cca01e01f9006206fd55194b208efedd5
Thomas Welborn <thomas.welborn@protonmail.com>
2019-11-04 09:37:01 -0800

    Add Tests Files

---
+++ tests/distribute/test-1/scripts/test-1-templates.js
@@ -0,0 +1,9 @@
+this["Tests"] = this["Tests"] || {};
+this["Tests"]["Test1"] = this["Tests"]["Test1"] || {};
+this["Tests"]["Test1"]["Ui"] = this["Tests"]["Test1"]["Ui"] || {};
+this["Tests"]["Test1"]["Ui"]["ModuleA"] = this["Tests"]["Test1"]["Ui"]["ModuleA"] || {};
+this["Tests"]["Test1"]["Ui"]["ModuleA"]["Template"] = `<div class="">
+
+</div>
+`;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInRlc3QtMS91aS9tb2R1bGUtYS90ZW1wbGF0ZS5lanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJ0ZXN0LTEtdGVtcGxhdGVzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiPGRpdiBjbGFzcz1cIlwiPlxyXG5cclxuPC9kaXY+XHJcbiJdfQ==
---
+++ tests/distribute/test-1/scripts/test-1.js
@@ -0,0 +1,10 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(factory());
+}(this, (function () { 'use strict';
+
+
+
+})));
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC0xLmpzIiwic291cmNlcyI6W10sInNvdXJjZXNDb250ZW50IjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7In0=
